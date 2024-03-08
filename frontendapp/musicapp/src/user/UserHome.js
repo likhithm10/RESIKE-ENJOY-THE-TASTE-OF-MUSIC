2dd4967495236d99bbeb91565fa3992077c4a096
LIKHITH MANNEPALLI <mannepallilikhith10@gmail.com>
2024-03-08 18:20:58 +0530

    Created user home

--- frontendapp/musicapp/src/user/UserHome.js
+++ frontendapp/musicapp/src/user/UserHome.js
@@ -1,7 +1,137 @@
 import React from 'react'
 
-export default function UserHome() {
+import './usernavbar.css'
+import logo from '../images/logo.png'
+import '../images/mainlogo.png'
+import './userhome.css'
+import hinana from '../images/hinana.jpg'
+import sidsriram from '../images/sidsriram.jpg'
+
+export default function userHome() {
   return (
-    <div>UserHome</div>
+    <div class="main_content">
+      <h2 align="right">
+        <input type="text"  name="search" placeholder="Search Albums,Artists, Geners etc..."/>&nbsp
+        </h2>
+        <div class="info">
+        
+<table>
+
+
+  {/* Albums */}
+
+<h2 align="left">Albums</h2><br/>
+  <tr>
+  
+    <td colSpan={10}>
+        <div className="card">
+        
+        <img src={hinana} alt='profile' width="50%"/>
+        
+        <h4>Hi Nanna</h4>
+        </div></td>
+        
+        
+        <td colSpan={2}><div className="card">
+        
+        <img src={hinana} alt='profile' width="50%"/>
+        
+        <h4>Hi Nanna</h4>
+        </div></td>
+        <td><div className="card">
+        
+        <img src={hinana} alt='profile' width="50%"/>
+        
+        <h4>Hi Nanna</h4>
+        </div></td>
+        <td><div className="card">
+        
+        <img src={hinana} alt='profile' width="50%"/>
+        
+        <h4>Hi Nanna</h4>
+        </div></td>
+        
+        </tr>
+
+
+{/* Artists */}
+
+        <h2 align="left">Artists</h2><br/>
+  <tr>
+  
+    <td colSpan={10}>
+        <div className="card">
+        
+        <img src={sidsriram} alt='profile' width="50%"/>
+        
+        <h4>Sidsriram</h4>
+        </div></td>
+        
+        
+        <td colSpan={2}><div className="card">
+        
+        <img src={sidsriram} alt='profile' width="50%"/>
+        
+        <h4>Sidsriram</h4>
+        </div></td>
+        <td><div className="card">
+        
+        <img src={sidsriram} alt='profile' width="50%"/>
+        
+        <h4>Sidsriram</h4>
+        </div></td>
+        <td><div className="card">
+        
+        <img src={sidsriram} alt='profile' width="50%"/>
+        
+        <h4>Sidsriram</h4>
+        </div></td>
+        
+        </tr>
+
+
+
+{/* Language */}
+
+        <h2 align="left">Language</h2><br/>
+  <tr>
+  
+    <td colSpan={10}>
+        <div className="card">
+        
+        <img src={logo} alt='profile' width="50%"/>
+        
+        <h4>Telugu</h4>
+        </div></td>
+        
+        
+        <td colSpan={2}><div className="card">
+        
+        <img src={logo} alt='profile' width="50%"/>
+        
+        <h4>Hindi</h4>
+        </div></td>
+        <td><div className="card">
+        
+        <img src={logo} alt='profile' width="50%"/>
+        
+        <h4>Tamil</h4>
+        </div></td>
+        <td><div className="card">
+        
+        <img src={logo} alt='profile' width="50%"/>
+        
+        <h4>Kannada</h4>
+        </div></td>
+        
+        </tr>
+        </table>
+
+        </div>
+
+
+      
+
+    </div>
   )
 }
