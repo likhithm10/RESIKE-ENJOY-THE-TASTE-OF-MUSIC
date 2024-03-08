d72fb43de173b261ef54b541af76c660479a8d94
LIKHITH MANNEPALLI <mannepallilikhith10@gmail.com>
2024-03-08 18:09:29 +0530

    Created About,login and signup for the main navbar

---
+++ frontendapp/musicapp/src/user/UserNavBar.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {Routes,Route,Link} from 'react-router-dom'
+import './usernavbar.css'
+import logo from '../images/logo.png'
+import '../images/Md.jpeg'
+import UserHome from './UserHome'
+import UserAlbums from './UserAlbums'
+import UserArtists from './UserArtists'
+import UserPlaylist from './UserPlaylist'
+import UserProfile from './UserProfile'
+import Home from '../main/Home'
+
+
+export default function UserNavBar() {
+  return (
+    <div class="wrapper">
+    <div class="sidebar">
+        <img className='row' src={logo} alt='logo' />
+        <br/> <br/> <br/>
+    <ul className='navbar'>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="/useralbums">Albums</Link></li>
+    <li><Link to="/userartists">Artists</Link></li>
+    <li><Link to="/userplaylist">My Playlist</Link></li>
+    <li><Link to="/userprofile">Profile</Link></li>
+    <li><Link to="/logout">Logout</Link></li>
+    </ul>
+    </div>
+    <Routes>
+        <Route path="/" Component={UserHome}/>
+        <Route path="/useralbums" Component={UserAlbums}/>
+        <Route path="/userartists" Component={UserArtists}/>
+        <Route path="/userplaylist" Component={UserPlaylist}/>
+        <Route path="/userprofile" Component={UserProfile}/>
+        <Route path="/logout" Component={Home}/>
+    </Routes>
+    
+</div>
+  )
+}
+
